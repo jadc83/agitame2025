d7c03e5b35660ddb3bcf3148f03c6a679d67655d
José Antonio Dominguez <joseantonio.dominguez@iesdonana.org>
2025-02-10 19:08:29 +0100

    Update 0.004

--- resources/views/components/comentario.blade.php
+++ resources/views/components/comentario.blade.php
@@ -1,12 +1,12 @@
-<div class="mt-2 p-2 bg-indigo-500 text-white rounded-lg w-full">
+<div class="mt-2 p-8 bg-indigo-500 text-white rounded-lg w-full">
     <div class="flex items-start space-x-3">
-        <p class="text-white font-medium text-lg w-full break-words">{{ $comentario->codigo }}: {{ $comentario->contenido }}</p>
+        <p class="p-4 rounded-xl font-medium text-lg w-full text-black bg-white">{{ $comentario->codigo }}: {{ $comentario->contenido }}</p>
     </div>
 
-    <div class="flex">
+    <div class="flex ">
         <form class="w-2/6 mt-4" action="{{ route('comentarios.store', ['comentable_type' => 'App\Models\Comentario', 'comentable_id' => $comentario->id]) }}" method="POST" >
             @csrf
-                <textarea name="respuesta" id="respuesta" rows="2" class="rounded-md w-10/12 m-4 text-black" placeholder="Escribe tu respuesta..."></textarea>
+                <textarea style="resize: none;" name="respuesta" id="respuesta" rows="2" class="rounded-md w-10/12 m-4 h-12 text-black" placeholder="Escribe tu respuesta..."></textarea>
                 <x-primary-button class="bg-orange-400 ml-4">Responder</x-primary-button>
 
         </form>
