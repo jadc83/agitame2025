bf5c2e06c461ee378ad56318c64ffa3c92b62d71
José Antonio Dominguez <joseantonio.dominguez@iesdonana.org>
2025-02-11 18:28:13 +0100

    Update 0.006

--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -38,7 +38,7 @@
                             <x-primary-button class="flex-shrink-0 py-2 px-4 w-full md:w-auto">Generar</x-primary-button>
                         </div>
                         </form>
-                        
+
 
                     <!--Menear y desmenear -->
                     <div class="flex items-center space-x-2 ml-auto">
